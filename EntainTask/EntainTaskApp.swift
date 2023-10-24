fb2c62f1437951bc4bbf27ae8c733c93c61f6b5e
Dipesh Dhakal <dipesh.dhakal4040@gmail.com>
2023-10-25 09:25:30 +1100

    Initial Commit

---
+++ EntainTask/EntainTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainTaskApp.swift
+//  EntainTask
+//
+//  Created by Dipesh Dhakal on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EntainTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
